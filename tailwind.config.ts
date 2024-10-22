cd506bc331fa5ffbbe8cd264e43da89e7a9550c2
Non-Stop Studio <roblox104roblox@gmail.com>
2024-10-22 18:49:24 +0200

    Added Dev dependency tailwind-scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
 import type { Config } from "tailwindcss";
+import tailwind_Scrollbar from "tailwind-scrollbar";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [tailwind_Scrollbar({ preferredStrategy: "pseudoelements" })],
 };
 export default config;
