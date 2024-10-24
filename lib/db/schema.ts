78309211ef4c24e219a0218f915c6bf77612170f
Non-Stop Studio <roblox104roblox@gmail.com>
2024-10-24 16:24:51 +0200

    New dependency bcrypt Added usersTable Schema.

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,1 +1,16 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	integer,
+	pgTable,
+	text,
+	timestamp,
+	varchar,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+	creationData: timestamp(),
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	username: varchar({ length: 255 }).notNull(),
+	profileImg_url: text(),
+	email: varchar({ length: 255 }).notNull().unique(),
+	password: varchar().notNull(),
+});
