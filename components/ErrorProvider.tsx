31eafff8814bc3e3b5fe9d32e0c7ecaaf93d19b5
Non-Stop Studio <roblox104roblox@gmail.com>
2024-11-01 20:51:18 +0100

    Wrapped error provider in a suspense

--- components/ErrorProvider.tsx
+++ components/ErrorProvider.tsx
@@ -2,13 +2,14 @@
 import { useTranslations } from "next-intl";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function ErrorProvider() {
 	const t = useTranslations("Errors");
 	const searchParams = useSearchParams();
 	const errorParam = searchParams.get("error");
 	return (
-		<>
+		<Suspense>
 			{errorParam != null && errorParam !== "" ? (
 				<Dialog modal defaultOpen>
 					<DialogContent>
@@ -21,6 +22,6 @@ export default function ErrorProvider() {
 			) : (
 				<></>
 			)}
-		</>
+		</Suspense>
 	);
 }
