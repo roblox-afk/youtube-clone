7480828b2b91dd7586c2314f605df12dc71bdd60
Non-Stop Studio <roblox104roblox@gmail.com>
2024-11-04 22:30:10 +0100

    Added redirect for uploadIconWidget

--- components/navigation/Icons/UploadIconWidget.tsx
+++ components/navigation/Icons/UploadIconWidget.tsx
@@ -8,9 +8,11 @@ import {
 } from "@/components/ui/popover";
 import { authClient } from "@/lib/auth/auth-client";
 import { SquarePen, Upload, Video } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function UploadIconWidget() {
 	const { data: session } = authClient.useSession();
+	const router = useRouter();
 	return (
 		<>
 			{session?.user != null ? (
@@ -23,14 +25,22 @@ export default function UploadIconWidget() {
 					<PopoverContent className="w-44 flex-col py-2 mr-4 px-0 bg-neutral-800 z-[105]">
 						<button
 							className="flex items-center px-4  flex-row h-10 hover:bg-neutral-700 relative w-full"
-							onClick={() => console.log("Button click: Upload video")}
+							onClick={() =>
+								router.push(
+									"/studio/channel/" + session.user.id + "/content/upload"
+								)
+							}
 						>
 							<Video strokeWidth={1} className="mr-4" />
 							<Label className="">Upload video</Label>
 						</button>
 						<button
 							className="flex items-center px-4  flex-row h-10 hover:bg-neutral-700 relative w-full"
-							onClick={() => console.log("Button click: Create post")}
+							onClick={() =>
+								router.push(
+									"/studio/channel/" + session.user.id + "/content/post"
+								)
+							}
 						>
 							<SquarePen strokeWidth={1} className="mr-4" />
 							<Label className="">Create post</Label>
